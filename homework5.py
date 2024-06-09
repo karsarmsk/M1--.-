e8ea7de412fd6f67ea55cd1c8bc2c2c13558e86b
Сергей Карасев <karasev-s@internet.ru>
2024-06-09 13:52:31 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 'Сергей', 10, 0.5, 'Karsar' # Создали кортеж (taple)
+print(immutable_var) # Вывели элементы кортежа на экран
+#immutable_var[0] = 'Sergey' # При изменении элемента: Ошибка - объект "кортеж" не поддерживает назначение элементов
+mutable_list = ['Сергей', 10, 0.5, 'Karsar'] # Создали список
+print(mutable_list) # Вывели элементы списка на экран
+mutable_list[0] = 'Sergey'
+print(mutable_list) # Вывели элементы списка на экран
